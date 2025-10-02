0b8e16859637625d17515a28fd8ef744a6a9f19f
Sabrina <scodipilly@scu.edu>
2025-10-02 10:45:50 +0100

    update scripts

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset;
+
     void Start()
     {
-        
+        offset = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 moveValue;
+    public float speed;
+
+    void OnMove(InputValue value)
     {
-        
+        moveValue = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
